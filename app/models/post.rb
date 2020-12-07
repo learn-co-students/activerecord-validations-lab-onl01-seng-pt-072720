e8eda10dece3204dc9f496d9da78bcac64d37c04
cinna-codes <somethingfellipickeditup@gmail.com>
2020-12-07 17:16:17 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with TitleValidator
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-fiction) }
 end
