e728df111585089f89752e8c73b38725930f96e0
agilles1 <adam.g.tpt@gmail.com>
2020-12-07 20:20:00 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,28 +3,28 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validate :is_clickbait?
-
-  CLICKBAIT_PATTERNS = [
-    /Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i
-  ]
-
-  def is_clickbait?
-    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
-      errors.add(:title, "must be clickbait")
-    end
-  end
-   
 #   validate :is_clickbait?
 
-#     def is_clickbait?
-#     if self.title.nil? ||!self.title.include?("Won't Believe") || !self.title.include?("Secret") || !self.title.include?("Top ") || !self.title.include?("Guess") 
-#         errors.add(:title, "not clickbaity enough!")  
+#   CLICKBAIT_PATTERNS = [
+#     /Won't Believe/i,
+#     /Secret/i,
+#     /Top [0-9]*/i,
+#     /Guess/i
+#   ]
+
+#   def is_clickbait?
+#     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+#       errors.add(:title, "must be clickbait")
 #     end
 #   end
+   
+  validate :is_clickbait?
+
+    def is_clickbait?
+        unless /Won't Believe/i.match?(self.title) || /Secret/i.match?(self.title) || /Top [0-9]*/i.match?(self.title) || /Guess/i.match?(self.title)
+            errors.add(:title, "not clickbaity enough!")  
+        end
+    end
 end
 
 # All posts have a title
