bff800483d538142e0c834f7c899d35134a1f40f
Kelin Field <kfield111@hotmail.com>
2020-12-15 18:40:44 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :must_be_clickbait_title
+
+    def must_be_clickbait_title
+        
+        title_strings = Array["Won't Believe", "Secret", "Top [number]", "Guess"]
+        
+        if title && title_strings.none? {|check_title| title.include?(check_title)}
+            errors.add(:bait_error, "Title must be clickbait.")
+        end
+    end
+
+
 end
